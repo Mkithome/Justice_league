525f87bd65efb5d2520bbac905cd57857b83295c
Martin Kithome <mkithome1@gmail.com>
2023-05-26 09:57:02 +0300

    create justice league and practice python

---
+++ justice_league.py
@@ -0,0 +1,14 @@
+countries = ['Croatia','USA','Argentina','France','Brazil','Japan','Vietnam','Israel']
+#for country in
+
+for index in [0,1,2,3,4,5,6,7,]:
+    print(index)
+    print(countries[index])
+
+
+
+ice_cream_flavours = ['Mint chocolate chip','Coffee','Cookie Dough','Fudge Mint']
+for ice_cream_flavour in ice_cream_flavours:
+        print('I love' + ice_cream_flavour + 'ice cream!!')
+
+        
